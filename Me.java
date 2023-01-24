371c878229763c42f05723d9ca18c46f38b570c5
Joseph Arif <josepharif3@gmail.com>
2023-01-24 20:52:57 +0200

    Joseph Arif, 18, Mechatronics engineering

---
+++ Me.java
@@ -0,0 +1,7 @@
+public class Me {
+	public static void main (String[] args){
+		System.out.println("Joseph Arif");
+		System.out.println("18");
+		System.out.print("Mechatronics engineering");
+	}
+}
